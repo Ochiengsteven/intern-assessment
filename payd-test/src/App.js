58ddbbdb836a5daa41aefe187560fd3056100cd8
Ochiengsteven <stevenochieng6305@gmail.com>
2024-06-12 16:58:23 +0300

    INITIALIZE APP AND INSTALL DEPENDENCIES

--- payd-test/src/App.js
+++ payd-test/src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { PhoneIcon } from "@chakra-ui/icons";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className="text-3xl text-blue-600 font-bold underline">
+          Hello world!
+        </h1>
+        <PhoneIcon w={6} h={6} color="green.500" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
